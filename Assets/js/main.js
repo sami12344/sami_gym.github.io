70b58aaeb37fe002a84cb4a1a5dd60f0ae324ffa
sami12344 <freelancersami16203@gmail.com>
2021-09-12 14:46:28 +0600

    glare fixed

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -16,9 +16,11 @@ window.onscroll = () => {
 }
 
 // vanilajs
-VanillaTilt.init(document.querySelector(".card"), {
+VanillaTilt.init(document.querySelectorAll(".card"), {
     max: 25,
-    speed: 400
+    speed: 400,
+    glare: true,
+    "max-glare": 1
 });
 
 //It also supports NodeList
