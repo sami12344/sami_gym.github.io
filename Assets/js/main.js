96be0f793a4825acf310ea1a2106227e140f736e
sami12344 <freelancersami16203@gmail.com>
2021-09-12 14:17:01 +0600

    vanila tilt

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -18,10 +18,8 @@ window.onscroll = () => {
 // vanilajs
 VanillaTilt.init(document.querySelector(".card"), {
     max: 25,
-    speed: 400,
-    glare: true,
-    "max-glare": 1,
-    
+    speed: 400
 });
 
 //It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".your-element"));
